7f7b955a76e6fbeb1ad35499ee83c615016a1702
dj566 <dj1531568@gmail.com>
2020-09-01 01:40:44 +0530

    sample-eclipse first commit

---
+++ sample-demo/src/main/java/com/sample/demo/Sample_Test.java
@@ -0,0 +1,11 @@
+package com.sample.demo;
+
+public class Sample_Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Sample-Demo");
+	}
+
+}
