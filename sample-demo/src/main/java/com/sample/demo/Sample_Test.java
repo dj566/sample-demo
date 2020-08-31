a09787f5882f021425acbdf9fbd04033e84f9f47
dj566 <dj1531568@gmail.com>
2020-09-01 02:22:55 +0530

    sample demo modified by A-3

--- sample-demo/src/main/java/com/sample/demo/Sample_Test.java
+++ sample-demo/src/main/java/com/sample/demo/Sample_Test.java
@@ -10,6 +10,8 @@ public class Sample_Test {
 		System.out.println("modified by developer B ");
 		System.out.println("modified by developer A ");
 		System.out.println("modified by developer B ");
+		System.out.println("modified by developer A ");
+
 		
 	}
 
